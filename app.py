38b2c44e55af891e948e42b8a6c82d0817700167
Yash Balkhande <78372068+balkhandeyash@users.noreply.github.com>
2023-11-07 17:41:07 +0530

    web app is ready

--- app.py
+++ app.py
@@ -4,23 +4,39 @@ import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-## load the Model
+# load the Model
 regmodel = pickle.load(open('linearRegModel.pkl', 'rb'))
-scaler = pickle.load(open('scaling.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods = ['POST'])
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(int(output[0]))
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)
+    if output == 0:
+        predicted_text = "The customer won't buy."
+    else:
+        predicted_text = "The customer will buy."
+    return render_template("home.html", predicted_text=predicted_text)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
